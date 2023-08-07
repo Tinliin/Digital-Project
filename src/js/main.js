201e34d89e31769c7f45967801f1f93ddddbe470
Александр Заяц <zayatc.av@gmail.com>
2023-08-07 23:34:23 +0300

    fixed bugs, improve flexability

--- src/js/main.js
+++ src/js/main.js
@@ -1,12 +1,7 @@
-// let burger = document.querySelector(".burger");
-// let burgerLine1 = document.querySelector(".burger__line1");
-// let burgerLine2 = document.querySelector(".burger__line2");
-// let burgerLine3 = document.querySelector(".burger__line3");
-// let nav = document.querySelector(".nav__list");
-// burger.addEventListener("click", ()=>{
-//     burger.classList.toggle("burger--active");
-//     nav.classList.toggle("nav__list--active");
-//     burgerLine1.classList.toggle("burger__line1--active");
-//     burgerLine2.classList.toggle("burger__line2--active");
-//     burgerLine3.classList.toggle("burger__line3--active");
-// })
+const burgerButton = document.querySelector('.burger-button');
+const navList = document.querySelector('.nav_list');
+
+burgerButton.addEventListener('click', () => {
+  burgerButton.classList.toggle('active');
+  navList.classList.toggle('active');
+});
