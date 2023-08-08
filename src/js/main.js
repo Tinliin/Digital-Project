ac57220df574224583eb322c205a37994f24b0db
Александр Заяц <zayatc.av@gmail.com>
2023-08-08 10:05:40 +0300

    made pseudo-placeholders disapiering on click, made white logo in footer

--- src/js/main.js
+++ src/js/main.js
@@ -5,3 +5,14 @@ burgerButton.addEventListener('click', () => {
   burgerButton.classList.toggle('active');
   navList.classList.toggle('active');
 });
+
+const form = document.querySelector('form');
+
+form.addEventListener('click', (e) => {
+  if (e.target.closest('.input_container')) {
+    const input_container = e.target.closest('.input_container');
+    const placeholder = input_container.querySelector('.placeholder');
+    placeholder.style.display = "none";
+  }
+
+});
